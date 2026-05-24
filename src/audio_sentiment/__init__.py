01294377fb3a3105609082323ee99a017ab6ed93
SaiManoj2003 <nvsaimanoj2003@gmail.com>
2026-05-24 18:12:40 +0000

    chore: initialise project structure and gitignore

---
+++ src/audio_sentiment/__init__.py
@@ -0,0 +1,8 @@
+"""
+Audio Call Sentiment Analysis
+------------------------------
+A multimodal pipeline that combines Whisper ASR, acoustic feature extraction,
+and dual-model emotion classification to analyse sentiment in call recordings.
+"""
+
+__version__ = "0.1.0"
